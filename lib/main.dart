6a3d20286199d355f26205c45ab220e93957ec9f
Beatriz <beatriz.sabbatini@gmail.com>
2021-10-26 23:14:51 -0300

    refactor/new-layout: Changed background image

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,14 @@ class _HomeState extends State<Home> {
   TextEditingController weightController = new TextEditingController();
   TextEditingController heightController = new TextEditingController();
 
-  String _message = "Digite seu peso e altura!";
+  String _message = "Digite seu peso e altura";
   String _resultMessage = "";
 
   void _resetFields() {
     weightController.text = "";
     heightController.text = "";
     setState(() {
-      _message = "Digite seu peso e altura!";
+      _message = "Digite seu peso e altura";
       _resultMessage = "";
       _formKey = GlobalKey<FormState>();
     });
@@ -70,81 +70,89 @@ class _HomeState extends State<Home> {
           ],
         ),
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            padding: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0.0),
-            child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Image(
-                          image: AssetImage('images/workout.jpg'),
-                          height: 220,
-                        )),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 20),
-                      child: Text(_message, textAlign: TextAlign.center, style: TextStyle(color: Colors.deepPurpleAccent[200], fontSize: 25.0, fontWeight: FontWeight.w300)),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)), focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 2)), labelText: "Peso (kg):", labelStyle: TextStyle(color: Colors.grey, fontSize: 20.0)),
-                      style: TextStyle(color: Colors.grey, fontSize: 20.0),
-                      textAlign: TextAlign.center,
-                      controller: weightController,
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return "Insira seu peso!";
-                        }
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)), focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 2)), labelText: "Altura (cm):", labelStyle: TextStyle(color: Colors.grey, fontSize: 20.0)),
-                      style: TextStyle(color: Colors.grey, fontSize: 20.0),
-                      textAlign: TextAlign.center,
-                      controller: heightController,
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return "Insira sua altura!";
-                        }
-                      },
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                      child: Container(
-                        height: 50.0,
-                        child: TextButton(
-                          onPressed: () {
-                            if (_formKey.currentState.validate()) _calculate();
-                          },
-                          child: Text(
-                            "Calcular",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white, fontSize: 25.00),
-                          ),
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurpleAccent[400]),
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50.0),
-                              ))),
+        body: Stack(
+          children: <Widget>[
+            Image.asset(
+              "images/background.png",
+              fit: BoxFit.cover,
+              height: 1000.0,
+            ),
+            SingleChildScrollView(
+                padding: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                            padding: EdgeInsets.only(top: 30, bottom: 5),
+                            child: Image(
+                              image: AssetImage('images/workout.png'),
+                              height: 220,
+                            )),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 20),
+                          child: Text(_message, textAlign: TextAlign.center, style: TextStyle(color: Colors.deepPurpleAccent[200], fontSize: 25.0, fontWeight: FontWeight.w300)),
                         ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-                      child: Text(_resultMessage, textAlign: TextAlign.center, style: TextStyle(color: Colors.deepPurpleAccent[200], fontSize: 25.0, fontWeight: FontWeight.w600)),
-                    )
-                  ],
-                ))));
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)), focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 2)), labelText: "Peso (kg):", labelStyle: TextStyle(color: Colors.grey, fontSize: 20.0)),
+                          style: TextStyle(color: Colors.grey, fontSize: 20.0),
+                          textAlign: TextAlign.center,
+                          controller: weightController,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Insira seu peso!";
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)), focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 2)), labelText: "Altura (cm):", labelStyle: TextStyle(color: Colors.grey, fontSize: 20.0)),
+                          style: TextStyle(color: Colors.grey, fontSize: 20.0),
+                          textAlign: TextAlign.center,
+                          controller: heightController,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Insira sua altura!";
+                            }
+                          },
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                          child: Container(
+                            height: 50.0,
+                            child: TextButton(
+                              onPressed: () {
+                                if (_formKey.currentState.validate()) _calculate();
+                              },
+                              child: Text(
+                                "Calcular",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white, fontSize: 25.00, fontWeight: FontWeight.w400),
+                              ),
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurpleAccent[400]),
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                  ))),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                          child: Text(_resultMessage, textAlign: TextAlign.center, style: TextStyle(color: Colors.deepPurpleAccent[200], fontSize: 25.0, fontWeight: FontWeight.w600)),
+                        )
+                      ],
+                    )))
+          ],
+        ));
   }
 }
